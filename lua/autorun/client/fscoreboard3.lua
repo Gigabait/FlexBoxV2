00c579a8a7219ccd6a7410d521e15499ccb5a664
Flex <LUModder@gmail.com>
2016-01-03 09:06:55 -0800

    Forgot PlayerColors table for ^ colored names

--- lua/autorun/client/fscoreboard3.lua
+++ lua/autorun/client/fscoreboard3.lua
@@ -16,6 +16,25 @@ surface.CreateFont("fs3_text",{
     weight = 500,
 })
 
+local PlayerColors = {
+    ["0"]  = Color(0,0,0),
+    ["1"]  = Color(128,128,128),
+    ["2"]  = Color(192,192,192),
+    ["3"]  = Color(255,255,255),
+    ["4"]  = Color(0,0,128),
+    ["5"]  = Color(0,0,255),
+    ["6"]  = Color(0,128,128),
+    ["7"]  = Color(0,255,255),
+    ["8"]  = Color(0,128,0),
+    ["9"]  = Color(0,255,0),
+    ["10"] = Color(128,128,0),
+    ["11"] = Color(255,255,0),
+    ["12"] = Color(128,0,0),
+    ["13"] = Color(255,0,0),
+    ["14"] = Color(128,0,128),
+    ["15"] = Color(255,0,255),
+}
+
 --cvars--
 local HostnameColorR    = CreateClientConVar("fs3_hostname_r",   0)
 local HostnameColorG    = CreateClientConVar("fs3_hostname_g",   150)
