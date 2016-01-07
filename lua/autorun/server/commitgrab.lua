5c610000ab15a9c49bc1b804262c37ec7f09b48c
Flex <LUModder@gmail.com>
2016-01-06 16:36:17 -0800

    Testing of multi-repo support

--- lua/autorun/server/commitgrab.lua
+++ lua/autorun/server/commitgrab.lua
@@ -5,50 +5,60 @@ Feel free to modify
 ]]--
 local Tag = "CommitGrab"
 CommitGrab = CommitGrab or {}
+CommitGrab.Repos = {
+	"B1IS/FlexBoxV2",
+	"LUModder/FWP",
+}
 
-http.Fetch("https://api.github.com/repos/B1IS/FlexBoxV2/commits",
-function(body)
-	CommitGrab.Data = util.JSONToTable(body)
-	CommitGrab.LatestCommit = CommitGrab.Data[1]
-end,
-function(err)
-	MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting data: "..err)
-end)
-
-function CommitGrab.GrabData()
-	http.Fetch("https://api.github.com/repos/B1IS/FlexBoxV2/commits",
+for _,repo in pairs(CommitGrab.Repos) do
+	http.Fetch("https://api.github.com/repos/"..repo.."/commits",
 	function(body)
-		CommitGrab.OldData = CommitGrab.Data
-		CommitGrab.Data = util.JSONToTable(body)
+		CommitGrab.Data[repo] = util.JSONToTable(body)
+		CommitGrab.LatestCommit[repo] = CommitGrab.Data[repo][1]
 	end,
 	function(err)
-		MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting data: "..err)
+		MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting data: "..err.."(repo: "..repo..")")
 	end)
+end
+
+function CommitGrab.GrabData()
+	for _,repo in pairs(CommitGrab.Repos) do
+		http.Fetch("https://api.github.com/repos/"..repo.."/commits",
+		function(body)
+			CommitGrab.OldData[repo] = CommitGrab.Data[repo]
+			CommitGrab.Data[repo] = util.JSONToTable(body)
+		end,
+		function(err)
+			MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting data: "..err.."(repo: "..repo..")")
+		end)
+	end
 end
 
 function CommitGrab.GrabCommitData()
-	http.Fetch("https://api.github.com/repos/B1IS/FlexBoxV2/commits/"..CommitGrab.LatestCommit.sha,
-	function(body)
-		CommitGrab.OldCommitData = CommitGrab.CommitData
-		CommitGrab.CommitData = util.JSONToTable(body)
-	end,
-	function(err)
-		MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting commit data: "..err)
-	end)
+	for _,repo in pairs(CommitGrab.Repos) do
+		http.Fetch("https://api.github.com/repos/"..repo.."/commits/"..CommitGrab.LatestCommit[repo].sha,
+		function(body)
+			CommitGrab.OldCommitData[repo] = CommitGrab.CommitData[repo]
+			CommitGrab.CommitData[repo] = util.JSONToTable(body)
+		end,
+		function(err)
+			MsgC(Color(100,200,100),"[CommitGrab] ",color_white,"Error getting data: "..err.."(repo: "..repo..")")
+		end)
+	end
 end
 
-function CommitGrab.AnnounceCommit()
+function CommitGrab.AnnounceCommit(repo)
 	for _,pl in pairs(player.GetAll()) do
-		pl:PrintMessage(3,"Latest FlexBox repo commit:")
-		pl:PrintMessage(3,"Commit "..CommitGrab.LatestCommit.sha.." by "..CommitGrab.LatestCommit.commit.author.name)
-		pl:PrintMessage(3,"\t"..CommitGrab.LatestCommit.commit.message)
-		pl:PrintMessage(3,"Files:")
-		for _,f in pairs(CommitGrab.CommitData.files) do
-			if f.additions == 0 then prefix = "D"
-			elseif f.deletions == 0 then prefix = "A"
-			else prefix = "U" end
-			pl:PrintMessage(3,prefix.."\t"..f.filename)
-		end
+			pl:PrintMessage(3,"Latest commit for "..repo..":")
+			pl:PrintMessage(3,"Commit "..CommitGrab.LatestCommit[repo].sha.." by "..CommitGrab.LatestCommit[repo].commit.author.name)
+			pl:PrintMessage(3,"\t"..CommitGrab.LatestCommit[repo].commit.message)
+			pl:PrintMessage(3,"Files:")
+			for _,f in pairs(CommitGrab.CommitData[repo].files) do
+				if f.additions == 0 then prefix = "D"
+				elseif f.deletions == 0 then prefix = "A"
+				else prefix = "U" end
+				pl:PrintMessage(3,prefix.."\t"..f.filename)
+			end
 	end
 end
 
@@ -56,11 +66,13 @@ timer.Create(Tag..".UpdateCommits",60,0,function()
 	CommitGrab.GrabData()
 	CommitGrab.GrabCommitData()
 	timer.Simple(10,function()
-		if CommitGrab.Data[1].sha != CommitGrab.LatestCommit.sha then
-			CommitGrab.LatestCommit = CommitGrab.Data[1]
-			timer.Simple(1,function()
-				CommitGrab.AnnounceCommit()
-			end)
+		for _,repo in pairs(CommitGrab.Repos) do
+			if CommitGrab.Data[repo][1].sha != CommitGrab.LatestCommit[repo].sha then
+				CommitGrab.LatestCommit[repo] = CommitGrab.Data[repo][1]
+				timer.Simple(1,function()
+					CommitGrab.AnnounceCommit(repo)
+				end)
+			end
 		end
 	end)
 end)
