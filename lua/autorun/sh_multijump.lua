ea699023e8d0d0336513e3c7f457e64860d75d5b
Flex <LUModder@gmail.com>
2016-01-08 10:05:21 -0800

    MultiJump
    WIP

---
+++ lua/autorun/sh_multijump.lua
@@ -0,0 +1,14 @@
+--[[
+MultiJump, based off of IJWTB's multi jump fly thing
+By Flex
+Feel free to use
+--]]
+local Tag = "MultiJump"
+
+if SERVER then
+
+end
+
+if CLIENT then
+    CreateClientConVar("cl_multijump",0)
+end
