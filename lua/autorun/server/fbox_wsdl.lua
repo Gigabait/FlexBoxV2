402075172b5a19684fbc8e4c02cd12482c9e381b
Flex <LUModder@gmail.com>
2016-01-21 17:09:22 -0800

    workshop dl

---
+++ lua/autorun/server/fbox_wsdl.lua
@@ -0,0 +1,29 @@
+resource.AddWorkshop( '173482196' ) -- SProps Workshop Edition
+resource.AddWorkshop( '104815552' ) -- SmartSnap
+resource.AddWorkshop( '108168164' ) -- Customisable Thirdperson
+resource.AddWorkshop( '104607712' ) -- Extended Properties
+resource.AddWorkshop( '104603291' ) -- Extended Spawnmenu
+resource.AddWorkshop( '104482086' ) -- [Official] Precision Tool
+resource.AddWorkshop( '150899249' ) -- Rp_City17 - Open Beta [Build 210]
+resource.AddWorkshop( '218917501' ) -- Simple Weather Content
+resource.AddWorkshop( '531849338' ) -- (Official) Gravity Hull Designator - Localized Physics & Infinite Maps
+resource.AddWorkshop( '133285346' ) -- Gm Grand Build
+resource.AddWorkshop( '243902601' ) -- gm_bluehills_test3
+resource.AddWorkshop( '104480013' ) -- Weight STool
+resource.AddWorkshop( '104818321' ) -- Buoyancy Tool
+resource.AddWorkshop( '107257490' ) -- Advanced Ball Socket Tool (Adv. Ballsocket)
+resource.AddWorkshop( '107382750' ) -- Smart Remover Tool
+resource.AddWorkshop( '107388767' ) -- Smart Freezer Tool
+resource.AddWorkshop( '136318146' ) -- Make Spherical
+resource.AddWorkshop( '107086721' ) -- Measuring Stick Tool
+resource.AddWorkshop( '251037184' ) -- Wire Fin Controller
+resource.AddWorkshop( '165509582' ) -- Fin 2
+resource.AddWorkshop( '140618773' ) -- gm_mobenix_v3_final_mp_fixed
+resource.AddWorkshop( '266666023' ) -- gm_genesis_b24
+resource.AddWorkshop( '546392647' ) -- Media Player
+resource.AddWorkshop( '169600867' ) -- GM_BigIsland
+resource.AddWorkshop( '138075711' ) -- gm_carconstruct_b1-8
+resource.AddWorkshop( '111929524' ) -- Multi-Parent / Unparent Tool
+resource.AddWorkshop( '132940295' ) -- rp_city8_2
+resource.AddWorkshop( '113352748' ) -- RP_City17_v1
+resource.AddWorkshop( '499775246' ) -- Rp_City17_District47
