67d9273fbbde044dd5fbc8a63c12e9e095c2dfe5
Flex <LUModder@gmail.com>
2016-01-24 11:46:06 -0800

    mapdata system

--- lua/autorun/lua_npc_metrocop.lua
+++ lua/autorun/lua_npc_metrocop.lua
@@ -51,50 +51,17 @@ if SERVER then
 	}
 
 
-	ENT.walktable = {
-		Vector(1450, -825, 384),
-		Vector(3124, -1412, 384),
-		Vector(2989, -875, 384),
-		Vector(1344, 845, 384),
-		Vector(49, 836, 376),
-		Vector(-762, 767, 376),
-		Vector(-2098, -104, 384),
-		Vector(-2887, -1396, 288),
-		Vector(-3675, -1977, 64),
-		Vector(-3139, -1150, 384),
-		Vector(-1710, -1569, 64),
-		Vector(2985, -1617, 64),
-		Vector(-3274, -1682, 64),
-		Vector(-2511, -1311, 384),
-		Vector(-3283, -1332, 384),
-		Vector(-2489, -561, 384),
-		Vector(-2876, -1753, 64),
-		Vector(-3429, -1753, 64),
-		Vector(-3419, -1647, 64),
-		Vector(-3550, -1735, 64),
-		Vector(1772, -782, 640),
-		Vector(687, -151, 384),
-		Vector(2096, -1097, 640),
-		Vector(2761, -1236, 560),
-		Vector(2273, -1092, 384),
-		Vector(2737, -1231, 384),
-	}
+	ENT.walktable = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].metrocops.walktable or {}
 
 
 	ENT.sittable = true
 
-	ENT.sittable = {
-
-	}
+	ENT.sittable = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].metrocops.sittable or {}
 
 
 
 	local function RandomNPC()
-		local spawns = {
-			Vector(1467, -1460, 384),
-			Vector(55, -1303, 384),
-			Vector(2986, -1618, 64),
-		}
+		local spawns = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].metrocops.spawns or {Vector(0,0,0),}
 		local npcs = ents.FindByClass("lua_npc_metrocop")
 		if #npcs > 10 then return end
 		local spawn = table.Random(spawns)
--- lua/autorun/lua_npc_wander.lua
+++ lua/autorun/lua_npc_wander.lua
@@ -171,97 +171,17 @@ if SERVER then
 	}
 
 
-	ENT.walktable = {
-		Vector(1450, -825, 384),
-		Vector(3124, -1412, 384),
-		Vector(2989, -875, 384),
-		Vector(1344, 845, 384),
-		Vector(49, 836, 376),
-		Vector(-762, 767, 376),
-		Vector(-2098, -104, 384),
-		Vector(-2887, -1396, 288),
-		Vector(-3675, -1977, 64),
-		Vector(-3139, -1150, 384),
-		Vector(-1710, -1569, 64),
-		Vector(2985, -1617, 64),
-		Vector(-3274, -1682, 64),
-		Vector(-2511, -1311, 384),
-		Vector(-3283, -1332, 384),
-		Vector(-2489, -561, 384),
-		Vector(-2876, -1753, 64),
-		Vector(-3429, -1753, 64),
-		Vector(-3419, -1647, 64),
-		Vector(-3550, -1735, 64),
-		Vector(1772, -782, 640),
-		Vector(687, -151, 384),
-		Vector(2096, -1097, 640),
-		Vector(2761, -1236, 560),
-		Vector(2273, -1092, 384),
-		Vector(2737, -1231, 384),
-	}
+	ENT.walktable = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].wanderer.walktable or {}
 
 
 	ENT.sittable = true
 
-	ENT.sittable = {
-		{
-			Vector(-1203, -31, 399),
-			Angle(0,-90,0),
-			false,
-		},
-		{
-			Vector(-63, -797, 399),
-			Angle(0,-90,0),
-			false,
-		},
-		{
-			Vector(125, -797, 399),
-			Angle(0,-90,0),
-			false,
-		},
-		--Train Station
-		{
-			Vector(-1536, -1682, 79),
-			Angle(0,90,0),
-			false,
-		},
-		{
-			Vector(-1787, -1682, 79),
-			Angle(0,90,0),
-			false,
-		},
-		{
-			Vector(-2061, -1682, 79),
-			Angle(0,90,0),
-			false,
-		},
-		--Train Station Tables
-		{
-			Vector(-2257, -1205, 80),
-			Angle(0,-180,0),
-			false,
-		},
-		{
-			Vector(-2349, -1205, 80),
-			Angle(0,0,0),
-			false,
-		},
-		--Apartments near train
-		{
-			Vector(1559, -769, 658),
-			Angle(0,0,0),
-			false,
-		},
-	}
+	ENT.sittable = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].wanderer.sittable or {}
 
 
 
 	local function RandomNPC()
-		local spawns = {
-			Vector(1467, -1460, 384),
-			Vector(1791, -342, 640),
-			Vector(-1148, -1198, 64),
-		}
+		local spawns = FBoxMapData[game.GetMap()] != nil and FBoxMapData[game.GetMap()].wanderer.spawns or {Vector(0,0,0),}
 		local npcs = ents.FindByClass("lua_npc_wander")
 		if #npcs > 20 then return end
 		local spawn = table.Random(spawns)
