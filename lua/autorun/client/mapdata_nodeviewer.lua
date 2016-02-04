247ed9732700c7a41f9d7cf81a6b38b83a7f578c
Flex <LUModder@gmail.com>
2016-02-03 19:51:54 -0800

    better node viewer fixed msitems_extra

--- lua/autorun/client/mapdata_nodeviewer.lua
+++ lua/autorun/client/mapdata_nodeviewer.lua
@@ -12,20 +12,24 @@ local NodeColor = {
 	[2] = Color(255,255,0),
 }
 
+
 local function MapNode(node,type,npc)
 	local t = type or 0
 	local t_str
 	if t == 0 then t_str = "Walk" elseif t == 1 then t_str = "Sit" elseif t == 2 then t_str = "Spawn" else t_str = "Unknown type!?!?!" end
 	local n = npc or 1
 	local color = NodeColor[t]
-	local lookatme = EyeAngles()
-	lookatme:RotateAroundAxis(lookatme:Right(),90)
-	lookatme:RotateAroundAxis(lookatme:Up(),-90)
-	cam.Start3D2D(node+Vector(0,0,30),lookatme,0.5)
-		draw.DrawText("Vector("..node.x..","..node.y..","..node.z..")","BudgetLabel",0,0,color,TEXT_ALIGN_CENTER)
-		draw.DrawText("Type: "..t_str,"BudgetLabel",0,-10,color,TEXT_ALIGN_CENTER)
-		draw.DrawText("NPC: "..(n == 1 and "Citizen" or "Metrocop"),"BudgetLabel",0,-20,color,TEXT_ALIGN_CENTER)
-	cam.End3D2D()
+	cam.Start2D()
+		local pos = node:ToScreen()
+		if LocalPlayer():EyePos():Distance(node) <= 750 then
+			draw.DrawText("Vector("..node.x..","..node.y..","..node.z..")","BudgetLabel",pos.x,pos.y,color,TEXT_ALIGN_CENTER)
+			if t == NODE_SIT then
+				draw.DrawText("Angle("..node.x..","..node.y..","..node.z..")","BudgetLabel",pos.x,pos.y-10,color,TEXT_ALIGN_CENTER)
+			end
+			draw.DrawText("Type: "..t_str,"BudgetLabel",pos.x,t == NODE_SIT and pos.y-20 or pos.y-10,color,TEXT_ALIGN_CENTER)
+			draw.DrawText("NPC: "..(n == 1 and "Citizen" or "Metrocop"),"BudgetLabel",pos.x,t == NODE_SIT and pos.y-30 or pos.y-20,color,TEXT_ALIGN_CENTER)
+		end
+	cam.End2D()
 
 	render.SetColorModulation(color.r/255,color.g/255,color.b/255)
 	local node_mdl = ClientsideModel("models/hunter/blocks/cube05x05x05.mdl",RENDERGROUP_OTHER)
@@ -40,7 +44,7 @@ hook.Add("PostDrawOpaqueRenderables","FBoxMapData.DrawNodes",function()
 	for _,node in pairs(FBoxMapData[game.GetMap()].metrocops.walktable) do
 		MapNode(node,NODE_WALK,NPC_METRO)
 	end
-	for _,tbl in pairs(FBoxMapData[game.GetMap()].wanderer.sittable) do
+	for _,tbl in pairs(FBoxMapData[game.GetMap()].metrocops.sittable) do
 		MapNode(tbl[1],NODE_SIT,NPC_METRO)
 	end
 	for _,node in pairs(FBoxMapData[game.GetMap()].metrocops.spawns) do
--- lua/autorun/msitems_extra.lua
+++ lua/autorun/msitems_extra.lua
@@ -1,1 +1,9 @@
+function IncludeSharedFile(v,func)
+	if SERVER and func then
+		if not func(v) then return end
+	end
+	if SERVER then AddCSLuaFile('msitems/'..v..'.lua') end
+	if CLIENT and not file.Exists('msitems/'..v..'.lua','LUA') then return end
+	include('msitems/'..v..'.lua')
+end
 IncludeSharedFile("sh_npcs")
