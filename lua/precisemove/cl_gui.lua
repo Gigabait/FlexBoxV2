58d2f6f13f130f6d6e4cde7eee8016e2003720e6
Flex <LUModder@gmail.com>
2016-02-04 13:55:15 -0800

    more wip

--- lua/precisemove/cl_gui.lua
+++ lua/precisemove/cl_gui.lua
@@ -1,3 +1,17 @@
-local PMFrame = vgui.Create("DFrame")
-PMFrame:SetSize(300,500)
-PMFrame:SetPos(ScrW()-20,ScrH()-20)
+function precise_gui()
+    local PMFrame = vgui.Create("DFrame")
+    PMFrame:SetSize(300,500)
+    PMFrame:SetPos(ScrW()-20,ScrH()-20)
+    PMFrame:SetTitle("Precise Movement")
+    PMFrame:SetIcon("icon16/arrow_out.png")
+    PMFrame:MakePopup()
+    
+    local target = vgui.Create("DButton",PMFrame)
+    target:Dock(TOP)
+    target:SetText("Select Target")
+    target.DoClick = target_select
+end
+
+local function target_select()
+    local select = vgui.Create("DFrame")
+end
