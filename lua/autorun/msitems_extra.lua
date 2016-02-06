04e0dc4eeb323122dd1dc4d55200cc89ddaccb88
Flex <lumodder@gmail.com>
2016-02-06 10:48:09 -0800

    ughghghghghghgh why is this such a pain

--- lua/autorun/msitems_extra.lua
+++ lua/autorun/msitems_extra.lua
@@ -1,9 +1,2 @@
-function IncludeSharedFile(v,func)
-	if SERVER and func then
-		if not func(v) then return end
-	end
-	if SERVER then AddCSLuaFile('msitems/'..v..'.lua') end
-	if CLIENT and not file.Exists('msitems/'..v..'.lua','LUA') then return end
-	include('msitems/'..v..'.lua')
-end
-IncludeSharedFile("sh_npcs")
+AddCSLuaFile("msitems/sh_npcs.lua")
+include("msitems/sh_npcs.lua")
