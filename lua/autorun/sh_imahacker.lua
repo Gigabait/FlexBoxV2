4ea83a40cacfe3182ecf85ad08d8b5b69c66f385
Flex <LUModder@gmail.com>
2016-03-07 13:59:15 -0800

    Update sh_imahacker.lua

--- lua/autorun/sh_imahacker.lua
+++ lua/autorun/sh_imahacker.lua
@@ -5,8 +5,13 @@ if SERVER then
         ply:Kick("baited")
     end)
 elseif CLIENT then
-    concommand.Add("imahacker",function(ply,args)
+    concommand.Add("cl_imahacker",function(ply,args)
         net.Start("imahacker")
+            net.WriteEntity(ply)
         net.SendToServer()
+    end,function(c,a)
+    local ac = {}
+    for _,ply in pairs(player.GetAll()) do
+        table.insert(ac,c..ply:Nick())
     end)
 end
