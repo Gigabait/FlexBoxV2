6171bb0078bbac444d55b3d40528078fe8e2107b
Flex <LUModder@gmail.com>
2016-03-07 09:29:41 -0800

    Update fbox_mapdata.lua

--- lua/autorun/fbox_mapdata.lua
+++ lua/autorun/fbox_mapdata.lua
@@ -1,16 +1,22 @@
 FBoxMapData = FBoxMapData or {}
 FBoxMapData["rp_city17_district47"] = {}
 
-local starttime = CurTime()
-
 local function mdata_print(txt)
 	MsgC(Color(0,150,130),"[Mapdata] ",Color(255,255,255),txt.."\n")
 end
 
-
-for _,file in pairs(file.Find("lua/mapdata/*","GAME")) do
-	include("mapdata/"..file)
-	mdata_print("Loaded "..file)
+function FBoxMapData.LoadMapdata()
+	local starttime = CurTime()
+	
+	for _,file in pairs(file.Find("lua/mapdata/*","GAME")) do
+		include("mapdata/"..file)
+		mdata_print("Loaded "..file)
+	end
+	
+	mdata_print("Mapdata loaded, took "..starttime-CurTime().."ms")
 end
 
-mdata_print("Mapdata loaded, took "..starttime-CurTime().."ms")
+aowl.AddCommand("reloadmapdata",function(ply,line)
+	mdata_print("Reload called by ",ply)
+	FBoxMapData.LoadMapdata()
+end)
