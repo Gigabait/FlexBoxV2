0474492c5c713072917a189e3ac25aff3ad33f62
Flex <LUModder@gmail.com>
2016-03-21 13:13:33 -0700

    Update homes.lua

--- lua/aowl_extended/homes.lua
+++ lua/aowl_extended/homes.lua
@@ -7,8 +7,10 @@ aowl.AddCommand("home",function(ply,line,vec1,vec2,vec3)
         elseif vec1 and vec2 and vec3 then
             ply:SetPData("fbox_home_pos",Vector(tonumber(vec1),tonumber(vec2),tonumber(vec3)))
         end
+aowl.GotoLocations["home"] = isvector(ply:GetPData("fbox_home_pos")) and ply:GetPData("fbox_home_pos") or aowl.GotoLocations["spawn"]
     elseif line == "remove" then
         ply:SetPData("fbox_home_pos","")
+aowl.GotoLocations["home"] = aowl.GotoLocations["spawn"]
     else
         if isvector(ply:GetPData("fbox_home_pos")) then
             ply:SetPos(ply:GetPData("fbox_home_pos"))
@@ -30,5 +32,3 @@ aowl.AddCommand("spawn", function(ply, line, target)
 		aowlMsg("spawn", tostring(ply).." spawned ".. (ent==ply and "self" or tostring(ent)))
 	end
 end)
-
-aowl.GotoLocations["home"] = isvector(ply:GetPData("fbox_home_pos")) and ply:GetPData("fbox_home_pos") or aowl.GotoLocations["spawn"]
