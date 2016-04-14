22b5249627996f31453f4751bff63593d10ab95e
Flex <lumodder@gmail.com>
2016-04-13 21:21:43 -0700

    fixes

---
+++ lua/mapdata/gm_buildersworld_finalv3/npc_nodes.lua
@@ -0,0 +1,129 @@
+FBoxMapData["gm_buildersworld_finalv3"].nodes = {
+	temp = {},
+	metrocops = {
+		spawns = {
+			Vector( 3987, -342, -1088 ),
+			Vector( 3872, -336, -1088 ),
+			Vector( 3872, -225, -1088 ),
+			Vector( 3974, -219, -1088 ),
+			Vector( 3980, -120, -1088 ),
+			Vector( 3865, -114, -1088 ),
+			Vector( 3846, -35, -1088 ),
+			Vector( 3952, -27, -1088 ),
+		},
+		walktable = {
+			Vector(181, -1984, -1144),
+			Vector(257, 3647, -1144),
+			Vector(5534, 3616, -1144),
+			Vector(5537, -5987, -1144),
+			Vector(-450, -6007, -1144),
+			Vector(-476, -2904, -1144),
+			Vector(132, -2917, -1144),
+			Vector(-1868, -2896, -1144),
+			Vector(-1891, -833, -1144),
+			Vector(-4259, -876, -1144),
+			Vector(-4268, 2729, -1144),
+			Vector(237, 2768, -1144),
+			Vector(4562, 2740, -1144),
+			Vector(4588, 913, -1144),
+			Vector(3878, 854, -1144),
+			Vector(3905, -818, -1144),
+			Vector(4419, -867, -1144),
+			Vector(4401, -2018, -1144),
+			Vector(801, 857, -440),
+			Vector(-1423, 892, -440),
+			Vector(-1415, -893, -440),
+			Vector(777, -850, -440),
+			Vector(780, -3362, -535),
+			Vector(-1286, -3306, -636),
+			Vector(-1253, -1178, -1144),
+			Vector(147, -1161, -1144),
+		},
+	},
+	wanderer = {
+		spawns = {
+			Vector( -2409, -3277, -636 ),
+			Vector( -2484, -3274, -636 ),
+			Vector( -2488, -3359, -636 ),
+			Vector( -2583, -3354, -636 ),
+			Vector( -2581, -3273, -636 ),
+		},
+		walktable = {
+			Vector(181, -1984, -1144),
+			Vector(257, 3647, -1144),
+			Vector(5534, 3616, -1144),
+			Vector(5537, -5987, -1144),
+			Vector(-450, -6007, -1144),
+			Vector(-476, -2904, -1144),
+			Vector(132, -2917, -1144),
+			Vector(-1868, -2896, -1144),
+			Vector(-1891, -833, -1144),
+			Vector(-4259, -876, -1144),
+			Vector(-4268, 2729, -1144),
+			Vector(237, 2768, -1144),
+			Vector(4562, 2740, -1144),
+			Vector(4588, 913, -1144),
+			Vector(3878, 854, -1144),
+			Vector(3905, -818, -1144),
+			Vector(4419, -867, -1144),
+			Vector(4401, -2018, -1144),
+			Vector(801, 857, -440),
+			Vector(-1423, 892, -440),
+			Vector(-1415, -893, -440),
+			Vector(777, -850, -440),
+			Vector(780, -3362, -535),
+			Vector(-1286, -3306, -636),
+			Vector(-1253, -1178, -1144),
+			Vector(147, -1161, -1144),
+		},
+	},
+	sittable = {
+		["models/props_c17/furniturecouch001a.mdl"] =
+		{
+			Vector( 40, 15, 10 ),
+			Vector( 40, -15, 10 ),
+		},
+		["models/props_trainstation/bench_indoor001a.mdl"] =
+		{
+			Vector( 32, 18, 0 ),
+			Vector( 32, -18, 0 ),
+		},
+		["models/props_trainstation/benchoutdoor01a.mdl"] =
+		{
+			Vector( 29, 24, 0 ),
+			Vector( 29, -24, 0 ),
+		},
+		["models/props_wasteland/controlroom_chair001a.mdl"] =
+		{
+			Vector( 34, 0, 0 ),
+		},
+		["models/props_c17/furniturecouch002a.mdl"] =
+		{
+			Vector( 34, 0, 0 ),
+		},
+		["models/props_combine/breenchair.mdl"] =
+		{
+			Vector( 31, 0, 0 ),
+		},
+		["models/props_c17/bench01a.mdl"] =
+		{
+			Vector( 29, 20, 0 ),
+			Vector( 29, -20, 0 ),
+		},
+		["models/props_c17/chair_office01a.mdl"] =
+		{
+			Vector( 2, 0, 0 ),
+		},
+		["models/props_trainstation/traincar_seats001.mdl"] =
+		{
+			Vector( 28, -40, 0 ),
+			Vector( 28, -15, 0 ),
+			Vector( 28, 15, 0 ),
+			Vector( 28, 40, 0 ),
+		},
+		["models/props_c17/chair02a.mdl"] =
+		{
+			Vector( 42, 0, 0 ),
+		},
+	},
+}
