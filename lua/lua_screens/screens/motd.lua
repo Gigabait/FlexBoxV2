c359c351a40e75cb1da0a87550334522d62ab191
Win7yes <francodemian@gmail.com>
2016-04-24 15:35:09 -0300

    MOTD Screen change
    Cause Flex said he liked it and that I could commit it.

--- lua/lua_screens/screens/motd.lua
+++ lua/lua_screens/screens/motd.lua
@@ -5,6 +5,17 @@ function draw.OutlinedBox(x,y,w,h,thick,col)
 	end
 end
 
+function draw.OutlinedBoxFilled( x, y, w, h, thickness, clr, fillclr )
+	surface.SetDrawColor( fillclr )
+	surface.DrawRect(x,y,w,h)
+	surface.SetDrawColor( clr )
+	for i=0, thickness - 1 do
+		surface.DrawOutlinedRect( x + i, y + i, w - i * 2, h - i * 2 )
+	end
+end
+
+
+
 ENT.Width	=	256
 ENT.Height 	=	128
 ENT.Scale	=	0.3
@@ -14,13 +25,36 @@ if SERVER then return end
 function SCREEN:create( ent )end
 
 function ENT:Draw3D2D(w,h)
-	draw.OutlinedBox(0,0,w,h,4,Color(0,150,130))
-	draw.RoundedBox(0,4,4,w-8,h-8,Color(0,0,0,200))
-	draw.DrawText("Welcome to FlexBox","DermaLarge",w/2+math.sin(RealTime()*2)*w/4,20+math.sin(RealTime()*5)*10,HSVToColor(RealTime()*50%360,1,1),TEXT_ALIGN_CENTER)
-	draw.DrawText("==Rules==","DermaLarge",w/2,50,Color(100,200,100),TEXT_ALIGN_CENTER)
-	draw.DrawText("1. Don't be a dick","DermaLarge",20,80,Color(255,255,255),TEXT_ALIGN_LEFT)
-	draw.DrawText("2. No prop spamming","DermaLarge",20,110,Color(255,255,255),TEXT_ALIGN_LEFT)
-	draw.DrawText("3. No laggy dupes","DermaLarge",20,140,Color(255,255,255),TEXT_ALIGN_LEFT)
-	draw.DrawText("4. No INTENTIONAL server crashing","DermaLarge",20,170,Color(255,255,255),TEXT_ALIGN_LEFT)
-	draw.DrawText("Managed by Flex and KaosHeaven, hosted by Sauermon","DermaLarge",w/2+math.sin(RealTime()*2)*w/12,h-50+math.sin(RealTime()*5)*-10,Color(0,150,130),TEXT_ALIGN_CENTER)
+	local x = math.Round(LocalPlayer():GetPlayerColor().x*255)
+	local y = math.Round(LocalPlayer():GetPlayerColor().y*255)
+	local z = math.Round(LocalPlayer():GetPlayerColor().z*255)
+	
+	local color_plycolor = Color(x,y,z)
+	
+	surface.SetDrawColor (0, 0, 0, 200)
+	surface.DrawRect (0, 0, w, h)
+	draw.OutlinedBox(0,0,w,h,7,color_white)
+	
+	surface.SetDrawColor(color_white)
+	surface.DrawRect(0,(12)-14/2,w,14)
+	
+	surface.SetDrawColor(Color(x,y,z))
+	surface.DrawRect(0,(10)-10/2,w,12)
+	
+	draw.DrawText("Hello "..UndecorateNick(LocalPlayer():GetName())..", Welcome to FlexBox","DermaLarge",w/2,h/10,color_plycolor,TEXT_ALIGN_CENTER)
+	draw.DrawText("There are "..tostring(#player.GetAll()).." players online","DermaLarge",w/2,h/6.5,color_plycolor,TEXT_ALIGN_CENTER)
+	
+	draw.OutlinedBoxFilled((w/10),(h/4),w/1.25,h/1.6,5,color_plycolor,Color(x,y,z,50))
+	surface.SetDrawColor(color_plycolor)
+	surface.DrawRect((w/2)-50,(h/4),100,33)
+	draw.DrawText("[RULES]","DermaLarge",w/2,h/4,Color(128,128,128),TEXT_ALIGN_CENTER)
+	
+	draw.DrawText("1. Don't be a dick","DermaLarge",w/9,h/3.5,Color(255,255,255),TEXT_ALIGN_LEFT)
+	draw.DrawText("2. No prop spamming","DermaLarge",w/9,h/2.8,Color(255,255,255),TEXT_ALIGN_LEFT)
+	draw.DrawText("3. No laggy dupes","DermaLarge",w/9,h/2.3,Color(255,255,255),TEXT_ALIGN_LEFT)
+	draw.DrawText("4. No INTENTIONAL server crashing","DermaLarge",w/9,h/1.95,Color(255,255,255),TEXT_ALIGN_LEFT)
+	
+	
+	draw.DrawText("Managed by Flex and KaosHeaven. Hosted by Sauermon","DermaLarge",w/2,h/1.15,color_plycolor,TEXT_ALIGN_CENTER)
+	draw.OutlinedBox(0,0,w,h,5,Color(x,y,z))
 end
