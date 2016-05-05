9f88763420a04061d43b91ac151f2bfd6cdee2c0
Flex <lumodder@gmail.com>
2016-05-04 18:30:21 -0700

    convert annoucnement system to hacky chat adding
    i really dont want to do github notifications with this

--- lua/autorun/sh_announcer.lua
+++ lua/autorun/sh_announcer.lua
@@ -3,7 +3,6 @@ util.AddNetworkString("fbox_announcer")
 	local msgs = {
 		{"Chat with us on ",Color(115,139,215),"Discord",Color(255,255,255),": ",Color(200,100,100),"http://discord.gg/0qPf8afP8tPllmm1"},
 		{"Seeing errors? Check the help menu by pressing ",Color(100,200,100),"F2",Color(255,255,255)," or typing ",Color(100,200,100),"!help",Color(255,255,255),"."},
-		{Color(200,100,100),"Please do not use the ",Color(200,200,100),"Layers ",Color(200,100,100),"tool it is currently broken."},
 	}
 	local delay = CurTime()
 	hook.Add("Think","FlexBoxAnnouncer",function()
@@ -17,12 +16,28 @@ util.AddNetworkString("fbox_announcer")
 end
 
 if CLIENT then
-	local noicons = CreateClientConVar("chat_rankicons", "1", true)
 
 	net.Receive("fbox_announcer",function()
 		local msg = util.JSONToTable(net.ReadString())
-		local time = os.date("*t")
-		local ic = noicons:GetBool() and "<texture=icon16/comments.png>" or ""
-		chat.AddText(Color(200,200,100),Format("%.2d:%.2d ",time.hour,time.min),Color(60,60,60),"\xe3\x80\x8a ",Color(255,255,255),ic,Color(220,70,100),"Announcement",Color(60,60,60)," \xe3\x80\x8b ",Color(255,255,255),unpack(msg))
+		local ic = "<texture=icon16/comments.png>"
+		local data = {Color(60,60,60),"\xe3\x80\x8a ",Color(255,255,255),ic,Color(220,70,100),"Announcement",Color(60,60,60)," \xe3\x80\x8b ",Color(255,255,255)}
+		for _,a in pairs(msg) do
+			table.insert(data,a)
+		end
+		chat.AddTimeStamp(data)
+
+		local data2 = {Color(60,60,60),"\xe3\x80\x8a ",Color(220,70,100),"Announcement",Color(60,60,60)," \xe3\x80\x8b ",Color(255,255,255)}
+		for _,a in pairs(msg) do
+			table.insert(data2,a)
+		end
+		table.insert(data2,"\n")
+		chat.AddTimeStamp(data2)
+
+		if chathud and chatgui and chatgui.Chat and chatgui.Chat.ChatLog then
+			chathud.AddText( unpack(data) )
+			chatgui.Chat.ChatLog.AddText(chatgui.Chat.ChatLog,unpack(data2))
+		else
+			chat.AddText(data)
+		end
 	end)
 end
