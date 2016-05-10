27ab44e9caa9bab3e3dcb3f7cdc650f57f01476d
Flex <LUModder@gmail.com>
2016-05-10 13:39:26 -0700

    add construct extended
    i doubt it'll let me commit with githubusercontent being blocked

--- lua/autorun/server/fbox_wsdl.lua
+++ lua/autorun/server/fbox_wsdl.lua
@@ -1,12 +1,13 @@
 local maps = {
-	gm_buildersworld_finalv3 = "117667617",
-	rp_city17_district47 = "499775246",
-	gm_construct_build_conquer_v2 = "536915311",
-	gm_genesis_b24 = "266666023",
-	gm_mega_flatgrass_v4 = "270479735",
-	gm_highway14800 = "152413570",
+	["gm_buildersworld_finalv3"] = "117667617",
+	["rp_city17_district47"] = "499775246",
+	["gm_construct_build_conquer_v2"] = "536915311",
+	["gm_genesis_b24"] = "266666023",
+	["gm_mega_flatgrass_v4"] = "270479735",
+	["gm_highway14800"] = "152413570",
 	["gm_carconstruct_b1-8"] = "138075711",
-	gm_infinite_concrete = "124358552",
+	["gm_infinite_concrete"] = "124358552",
+	["gm_construct_extended"] = "509873419",
 }
 
 resource.AddWorkshop(maps[game.GetMap()])
